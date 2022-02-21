1e530529a6712883080b9bc451db1c0a016528e0
Login Jones <login.jones@gmail.com>
2022-02-21 16:23:04 -0500

    initial routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import About from './About/About';
+import Main from './Main/Main'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/' element={ <Main /> }></Route>
+          <Route path='/about' element={ <About /> }></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
