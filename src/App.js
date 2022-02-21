7b8734728ba9e6537cc0989aae8f0b2a418f7fd2
Login Jones <login.jones@gmail.com>
2022-02-21 16:42:01 -0500

    links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import About from './About/About';
 import Main from './Main/Main'
 
 function App() {
   return (
     <div className="App">
+
       <Router>
+        <ul>
+          <li><Link to='/'>Main</Link></li>
+          <li><Link to='/about'>About</Link></li>
+        </ul>
         <Routes>
           <Route path='/' element={ <Main /> }></Route>
           <Route path='/about' element={ <About /> }></Route>
